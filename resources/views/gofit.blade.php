c1f38d791bc07b816f21753e9d8ae2be80cf1e43
dlo <dlo@DLo-ThinkPad-P52s-Ubuntu>
2019-01-08 10:21:20 -0500

    Add sample workouts markup.

--- resources/views/gofit.blade.php
+++ resources/views/gofit.blade.php
@@ -41,6 +41,55 @@
                 <a class="btn btn-primary btn-lg disabled" href="#" role="button">Add A Workout</a>
               </p>
             </div>
+            <h2>Workouts</h2>
+            <div class="table-responsive">
+                <table id="workouts" class="table">
+                    <thead>
+                        <tr>
+                          <th scope="col">Workout</th>
+                          <th scope="col">Cycle</th>
+                          <th scope="col">Date</th>
+                          <th scope="col">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><h3 class="text-nowrap">Cardio</span></h3>
+                            <td>
+                                <div class="text-nowrap">
+                                    <span class="badge badge-secondary">Week 1</span>
+                                    <span class="badge badge-secondary">Day 2</span>
+                                </div>
+                                <span class="badge badge-info">Cycle 1</span>
+                            </td>
+                            <td>2019/01/01</td>
+                            <td>
+                                <div class="text-nowrap">
+                                    <button type="button" class="btn btn-info">Edit</button>
+                                    <button type="button" class="btn btn-danger">Delete</button>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><h3 class="text-nowrap">531 Bench</h3></td>
+                            <td>
+                                <div class="text-nowrap">
+                                    <span class="badge badge-secondary">Week 1</span>
+                                    <span class="badge badge-secondary">Day 1</span>
+                                </div>
+                                <span class="badge badge-info">Cycle 1</span>
+                            </td>
+                            <td>2018/12/28</td>
+                            <td>
+                                <div class="text-nowrap">
+                                    <button type="button" class="btn btn-info">Edit</button>
+                                    <button type="button" class="btn btn-danger">Delete</button>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </main>
     <script>
