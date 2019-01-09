c0e0344858ea6eb9640e9e843485288aea3dd71d
dlo <dlo@DLo-ThinkPad-P52s-Ubuntu>
2019-01-08 21:57:00 -0500

    Comment versioning in webpack mix file.

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,6 +14,7 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css');
 
+// Version / cache-bust compiled assets
 if (mix.inProduction()) {
     mix.version();
 }
